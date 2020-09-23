c5d4e68ffc654bd6a0c7d9d55b6051762258f711
BuildTools <unconfigured@null.spigotmc.org>
2020-09-23 17:22:25 +0200

    Funzioni di gioco aggiunte

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
-var config={ }
+var config={
+    type: Phaser.AUTO,
+    width:900,
+    height: 700,
+    scene:{
+	preload: preload,
+	create: create,
+	update: update
+    }
+
+ };
 var game = new Phaser.Game(config);
 
+function preload() {
+   //
+}
+
+function create() {
+    //
+}
+
+function update() {
+    //
+}
