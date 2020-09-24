cded69bbde8d67ccb7c3db76ab4f927f98cc621e
BuildTools <unconfigured@null.spigotmc.org>
2020-09-24 21:41:37 +0200

    Agginuto

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var config={
     type: Phaser.AUTO,
-    width:850,
-    height: 650,
+    width:1920,
+    height: 1080,
     scene:{
 	preload: preload,
 	create: create,
@@ -12,11 +12,12 @@ var config={
 var game = new Phaser.Game(config);
 
 function preload() {
-   //
+   this.load.image('map', '../maps/sheld.png');
+   this.load.spritesheet('playerSprite', '../animazioni/3D/Run_begin/3D_spritesheet_run_begin.png', { frameWidth: 64, frameHeight: 64 } );
 }
 
 function create() {
-    //
+    this.add.image(0,0,'map').setOrigin(0,0);
 }
 
 function update() {
